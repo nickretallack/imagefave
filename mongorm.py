4e51553f5f72bd319fdc4bb9d5bab439f9e61aba
Nick Retallack <nickretallack@gmail.com>
2010-11-20 03:51:53 -0800

    now the files get saved in mongodb gridfs

---
+++ mongorm.py
@@ -0,0 +1,95 @@
+from UserDict import DictMixin
+
+# Todo: make this a threadlocal
+identity_map = {}
+
+def identity_map_hit(result):
+    return result
+
+class Record(DictMixin):
+    def __new__(class_, *args, **kwargs):
+        # TODO: handle the case of ObjectId vs non-ObjectId in the _id field
+        if '_id' in kwargs and kwargs['_id'] in identity_map:
+            return identity_map[id]
+
+        instance = super(Record, class_).__new__(class_)
+
+        if '_id' in kwargs:
+            identity_map[id] = instance
+
+        return instance
+
+    def __init__(self, **kwargs):
+        self.__dict__['_data'] = kwargs
+
+    def __repr__(self, **kwargs):
+        return "<%s %s>" % (self.__class__, self._data)
+
+    @classmethod
+    def _resolve_collection(cls, collection=None):
+        if not collection:
+            collection = getattr(cls, '_collection', None)
+            if not collection:
+                raise Exception, "No collection selected"
+
+        # Collection may be a name, or an actual collection
+        if isinstance(collection, basestring):
+            collection = db[collection]
+
+        return collection
+
+    def save(self, _collection=None):
+        collection = self._resolve_collection(_collection)
+        id = collection.save(self._data)
+        identity_map[id] = self
+        return id
+
+    @classmethod
+    def find(cls, _collection=None, **kwargs):
+        collection = cls._resolve_collection(_collection)
+        results = collection.find(kwargs)
+        return [cls(**result) for result in results]
+
+    @classmethod
+    def find_one(cls, _collection=None, **kwargs):
+        collection = cls._resolve_collection(_collection)
+        result = collection.find_one(kwargs)
+        if result:
+            return cls(**result)
+
+    @classmethod
+    def count(cls, _collection=None, **kwargs):
+        collection = cls._resolve_collection(_collection)
+        return collection.count(kwargs)
+
+    # Dict Mixin Stuff
+    def __getitem__(self, key):
+        return self._data[key]
+
+    def __setitem__(self, key, value):
+        self._data[key] = value
+
+    def __delitem__(self, key):
+        del self._data[key]
+
+    def keys(self):
+        return self._data.keys()
+
+    # Pretty Properties
+
+    def __getattr__(self, attr):
+        if attr in self._data:
+            return self._data[attr]
+        else:
+            raise AttributeError
+
+    def __setattr__(self, attr, value):
+        self._data[attr] = value
+
+if __name__ == "__main__":
+    class User(Record):
+        _collection = 'user'
+    a = User(name='bob')
+    a.save()
+    b = User.find_one(name='bob')
+    #assert a == b
--- receiver.py
+++ receiver.py
@@ -1,12 +1,52 @@
 from flask import Flask
 app = Flask(__name__)
 from urllib import urlopen
+from hashlib import sha1 as hasher
 
-@app.route("/fave/<user>/<path:data>")
-def fave(user, data):
-    output = open("foo.png","wb")
-    output.write(urlopen(data).read())
-    app.logger.debug("Hit with %s" % data)
+import mongorm
+from pymongo import Connection
+from gridfs import GridFS
+from gridfs.errors import FileExists
+
+connection = Connection()
+db = mongorm.db = connection.imagefave
+fs = GridFS(db)
+
+class User(mongorm.Record):
+    _collection='user'
+    # schema: id=user_id
+
+class Preference(mongorm.Record):
+    _collection='preference'
+    # schema: user_id, image_id
+
+#class Image(Record):
+#    _collection='image'
+#    # schema: id=image_id, extension
+
+
+
+@app.route("/fave/<user>/<path:upload_path>")
+def fave(user, upload_path):
+    upload_data = urlopen(upload_path).read()
+    image_id = hasher(upload_data).hexdigest()
+    image_extension = upload_path.rsplit('.',1)[1]
+    preference_data = {'user_id':user,'image_id':image_id}
+    if not Preference.find_one(**preference_data):
+        Preference(**preference_data).save()
+    
+    try:
+        # Note: this can stream from a file.  We should do that.
+        fs.put(upload_data, _id=image_id)
+    except FileExists:
+        pass
+
+    #if not Image.find_one({'_id':image_id}):
+    #    Image(_id=image_id, extension=image_extension).save()
+
+    #output = open("foo.png","wb")
+    #output.write(upload_data)
+    #app.logger.debug("Hit with %s" % data)
     return ""
 
 if __name__ == "__main__":
